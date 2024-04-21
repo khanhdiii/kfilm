8a1c3c38d79cf2c7f793df642dc1eda021bfbfb9
khanhdiii <khanh12369874@gmail.com>
2024-04-22 03:19:13 +0700

    fix: delete exact and add path /viewall

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
-import MovieDetails from "./components/MovieDetails";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
 import SearchMovies from "./components/SearchMovies";
 import Header from "./components/Header/Header";
+import ViewAll from "./components/ViewAll/ViewAll";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetails />} />
         <Route path="/search" element={<SearchMovies />} />
+        <Route path="/viewall" element={<ViewAll />} />
       </Routes>
     </Router>
   );
