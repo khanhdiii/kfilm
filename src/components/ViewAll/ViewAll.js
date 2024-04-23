02827d1947bdb5526ba2a069f29bf9e4339bc4e2
khanhdiii <khanh12369874@gmail.com>
2024-04-24 06:45:59 +0700

    fix: style and add new pagination lib in code

--- src/components/ViewAll/ViewAll.js
+++ src/components/ViewAll/ViewAll.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import ReactPaginate from "react-paginate";
 import { useLocation, useNavigate } from "react-router-dom";
+
 import fetchAPI, { URL_FILM } from "../../utils/fetchAPI";
-import "./viewall.scss";
+
 import Header from "../Header/Header";
+import "./viewall.scss";
+import "../scss/base.scss";
 
 function ViewAll() {
   const location = useLocation();
   const navigate = useNavigate();
   const [movies, setMovies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   const limit = 20;
 
   useEffect(() => {
@@ -22,6 +27,7 @@ function ViewAll() {
     try {
       const response = await fetchAPI(`${api}?page=${page}&limit=${limit}`);
       setMovies(response.data.items);
+      setTotalPages(response.data.params.pagination.totalPages);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -34,49 +40,45 @@ function ViewAll() {
   }, [currentPage, location]);
 
   const handlePaginationChange = (page) => {
-    setCurrentPage(page);
+    setCurrentPage(page.selected + 1); // +1 vì page.selected bắt đầu từ 0
   };
 
   return (
-    <div className="view-all">
+    <div className="main-page">
       <Header />
-      <h2>All Movies</h2>
-      <ul className="movie-list">
-        {movies &&
-          movies.map((movie) => (
-            <div key={movie._id} className="movie-item">
-              <li className="movie-content">
-                <Link to={`/detailmovie/${movie.slug}`}>
-                  <img
-                    className="movie-image"
-                    src={`${URL_FILM}/${movie.poster_url}`}
-                    alt={movie.name}
-                  />
-                </Link>
-                <Link to={`/detailmovie/${movie.slug}`}>
-                  <h3 className="movie-name">{movie.name}</h3>
-                </Link>
-                <p className="movie-year">{movie.year}</p>
-              </li>
-            </div>
-          ))}
-      </ul>
-
-      <div className="pagination">
-        <button
-          className="pagination-btn"
-          disabled={currentPage === 1}
-          onClick={() => handlePaginationChange(currentPage - 1)}
-        >
-          Previous
-        </button>
-        <span className="pagination-page">{currentPage}</span>
-        <button
-          className="pagination-btn"
-          onClick={() => handlePaginationChange(currentPage + 1)}
-        >
-          Next
-        </button>
+      <div className="content">
+        <h2 className="title-name">All Movies</h2>
+        <ul className="movie-list">
+          {movies &&
+            movies.map((movie) => (
+              <div key={movie._id} className="movie-item">
+                <li className="movie-content">
+                  <Link to={`/detailmovie/${movie.slug}`}>
+                    <img
+                      className="movie-image"
+                      src={`${URL_FILM}/${movie.poster_url}`}
+                      alt={movie.name}
+                    />
+                  </Link>
+                  <Link to={`/detailmovie/${movie.slug}`}>
+                    <h3 className="movie-name">{movie.name}</h3>
+                  </Link>
+                  <p className="movie-year">{movie.year}</p>
+                </li>
+              </div>
+            ))}
+        </ul>
+        <section className="pagination-container">
+          {/* Hiển thị phân trang */}
+          <ReactPaginate
+            pageCount={totalPages}
+            onPageChange={handlePaginationChange}
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            containerClassName={"pagination"}
+            activeClassName={"active"}
+          />
+        </section>
       </div>
     </div>
   );
